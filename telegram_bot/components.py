98540ef5022ae3ab212980b732beb34fd81a5c0d
neotod <neotod@protonmail.com>
2021-06-21 23:32:54 +0430

    adding event class

--- telegram_bot/components.py
+++ telegram_bot/components.py
@@ -1,3 +1,12 @@
+import enum
+
+
+class Events(enum.Enum):
+    lock_state_change = enum.auto()
+    user_login = enum.auto()
+    user_change = enum.auto()
+    entry = enum.auto()
+
 class Smart_Lock:
     def __init__(self, state: bool=True, ontime_report_on: bool=False, longtime_report_on: bool=False, phone_number: int=0):
         self.on = state # true => lock is working, false => lock is not working
@@ -6,8 +15,14 @@ class Smart_Lock:
         self.phone_number = phone_number
 
 class User:
-    def __init__(self, name: str, lastname: str, username:str, phone_number :int=0):
+    def __init__(self, telegram_obj, name: str, lastname: str, username:str, phone_number :int=0):
+        self.telegram_obj = telegram_obj
         self.name = name
         self.lastname = lastname
         self.username = username
         self.phone_number = phone_number
+
+class Event:
+    def __init__(self, id_: Events, name: str):
+        self.id_ = id_
+        self.name = name
