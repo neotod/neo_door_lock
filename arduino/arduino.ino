c16d288a9d75d13dbc75c0605ddebedc3934fc0d
neotod <neotod@protonmail.com>
2021-07-17 13:00:19 +0430

    adding sim800l module part (sending sms)

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,19 +1,29 @@
 #include <ArduinoJson.h>
+#include <SoftwareSerial.h>
 
-#define LOCK_PIN 4
+#define GSM_RX 2
+#define GSM_TX 3
+
+#define LOCK_PIN 7
 
 bool data_ready = false;
 String data = "";
 
+SoftwareSerial gsm_serial(GSM_RX, GSM_TX);
 
 void setup() {
     Serial.begin(115200);
+    gsm_serial.begin(9600);
 
     pinMode(LOCK_PIN, OUTPUT);
 }
 
 void loop() {
     if (Serial.available()) {
+        digitalWrite(LOCK_PIN, HIGH);
+        delay(100);
+        digitalWrite(LOCK_PIN, LOW);
+        
         data = Serial.readString();
         data_ready = true;
     }
@@ -30,14 +40,37 @@ void loop() {
         if (doc["type"] == "request") {
             doc["type"] = "response";
 
-            if (doc["data"] == "lock") {
-                doc["data"] = "done";
+            if (doc["module"] == "lock") {
+                doc["result"] = "done";
                 delay(50);
                 serializeJson(doc, Serial);
 
                 digitalWrite(LOCK_PIN, HIGH);
                 delay(3000);
                 digitalWrite(LOCK_PIN, LOW);
+            }
+            else if (doc["module"] == "gsm") {
+                doc["result"] = "done";
+                delay(50);
+                serializeJson(doc, Serial);
+                delay(500);
+
+                String number = doc["number"];
+                String send_inst = "AT+CMGS=\"" + number + "\"";
+                String text = doc["text"];
+                
+                gsm_serial.println("AT");
+                delay(100);
+              
+                gsm_serial.println("AT+CMGF=1");
+                delay(100);
+                gsm_serial.println(send_inst);
+                delay(100);
+                gsm_serial.print(text);
+                delay(100);
+                gsm_serial.write(26);
+                
+                delay(500);
             }
 
             data_ready = false;
