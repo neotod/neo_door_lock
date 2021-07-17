6c3c1319453d387a6f5a603bea24099fe792681f
neotod <neotod@protonmail.com>
2021-07-17 13:01:17 +0430

    adding sim800l part; wifi connecting part; changes in lock request handling part

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -13,12 +13,18 @@
 
 void index_handle();
 void lock_handle();
+void gsm_handle();
+void wifi_handle();
 
 
 ESP8266WebServer server(WEBSERVER_PORT);
 
 const char* AP_SSID = "ESP";
 const char* AP_PASS = "ThisIsESP8266";
+
+int WIFI_IP[4] = {192, 168, 1, 20};
+int WIFI_GATE[4] = {192, 168, 1, 9};
+int WIFI_SUB[4] = {255, 255, 255, 0};
 
 
 void setup()
@@ -31,7 +37,9 @@ void setup()
 
     server.on("/", HTTP_GET, index_handle);
     server.on("/index", HTTP_GET, index_handle);
-    server.on("/lock", HTTP_POST, lock_handle);
+    server.on("/lock", HTTP_GET, lock_handle);
+    server.on("/gsm", HTTP_POST, gsm_handle);
+    server.on("/wifi", HTTP_POST, wifi_handle);
 
     server.begin();
 }
@@ -51,16 +59,49 @@ void index_handle() {
 }
 
 void lock_handle() {
-    if (!server.hasArg("value")) {
-        server.send(HTTP_BAD_REQUEST);
-        return;
-    }
-
-    String value = server.arg("value");
+    digitalWrite(GPIO2, HIGH);
+    delay(300);
+    digitalWrite(GPIO2, LOW);
 
     StaticJsonDocument<256> doc;
     doc["type"] = "request";
-    doc["data"] = value;
+    doc["module"] = "lock";
+    doc["result"] = "";
+    serializeJson(doc, Serial);
+
+    delay(1000);
+    bool result = confirmation_wait();
+
+    if (result)
+        server.send(200);
+    else
+        server.send(HTTP_INTERNAL_SERVER_ERROR);
+}
+
+void gsm_handle() {
+    if (!server.hasArg("text")) {
+        server.send(HTTP_BAD_REQUEST, "text/html", "please provide text");
+        return;
+    }
+    else if (!server.hasArg("number")) {
+        server.send(HTTP_BAD_REQUEST, "text/html", "please provide number => ex: 9035315000");
+        return;
+    }
+
+    String text = server.arg("text");
+    String number = server.arg("number");
+
+    if (number.length() != 10) {
+        server.send(HTTP_BAD_REQUEST, "text/html", "please provide a valid number => ex: 9035315000");
+        return;
+    }
+
+    StaticJsonDocument<512> doc;
+    doc["type"] = "request";
+    doc["module"] = "gsm";
+    doc["text"] = text;
+    doc["number"] = number;
+    doc["result"] = "";
     serializeJson(doc, Serial);
 
     bool result = confirmation_wait();
@@ -69,6 +110,32 @@ void lock_handle() {
         server.send(200);
     else
         server.send(HTTP_INTERNAL_SERVER_ERROR);
+}
+
+void wifi_handle() {
+    if (!server.hasArg("ssid")) {
+        server.send(HTTP_BAD_REQUEST, "text/html", "please provide the ssid");
+        return;
+    }
+    else if (!server.hasArg("password")) {
+        server.send(HTTP_BAD_REQUEST, "text/html", "please provide the password");
+        return;
+    }
+  
+    const char* wifi_ssid = server.arg("ssid").c_str();
+    const char* wifi_pass = server.arg("password").c_str();
+    
+    server.send(200, "text/html", "Please wait until ESP changes its mode to Station mode.");
+
+    if (WiFi.getMode() != WIFI_STA)
+        WiFi.mode(WIFI_STA);
+
+    IPAddress staticIP(WIFI_IP[0], WIFI_IP[1], WIFI_IP[2], WIFI_IP[3]);
+    IPAddress gateway(WIFI_GATE[0], WIFI_GATE[1], WIFI_GATE[2], WIFI_GATE[3]);
+    IPAddress subnet(WIFI_SUB[0], WIFI_SUB[1], WIFI_SUB[2], WIFI_SUB[3]);
+
+    WiFi.config(staticIP, gateway, subnet);
+    WiFi.begin(wifi_ssid, wifi_pass);
 }
 
 bool confirmation_wait() {
@@ -97,7 +164,7 @@ bool confirmation_wait() {
             }
             else {
                 if (doc["type"] == "response") {
-                    if (doc["data"] == "done") {
+                    if (doc["result"] == "done") {
                         success = true; // success, redirect to home
                         break;
                     }
